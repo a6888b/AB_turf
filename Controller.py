02684ab09d3c655179e2b3892d1d17e45933c529
a6888b <ayoubbrahim68@gmail.com>
2021-12-02 22:18:33 +0100

    recupere bien tout les match du jour est ce format a la perf

--- Controller.py
+++ Controller.py
@@ -19,40 +19,39 @@ class Control:
         self.nombre_victoires = []
 
 
-    def save_data(self, data: dict):
+    def save_data(self, data: list):
         with open(f"result_scrap.json", "a", encoding="utf-8") as f_data:
             json.dump(data, f_data, indent=1, ensure_ascii=False)
 
 
     def get_data(self, course: list[str]):  # recupere les donne de l'api
+        data = []
         for index, number in enumerate(course):
-            data = {"heure": None, "number": None, "nom_course": None, "participants":
-                    {"nom": [], "age": [], "numPmu": [], "nombre course": [], "nombre placée": [], "nombre placée second": [], "nombre placée troisieme": [], "nombre victoire": [], "chance de gagner": [], "chance_gagner_place":[]}}
+            data.append({"heure": None, "number": None, "nom_course": None, "participants":
+                    {"nom": [], "age": [], "numPmu": [], "nombre course": [], "nombre placée": [], "nombre placée second": [], "nombre placée troisieme": [], "nombre victoire": [], "chance de gagner": [], "chance_gagner_place":[]}})
+                    
             url = f"{Model.URL_API+Model.DATE_URL}/{number.upper()[:-2]}/{number.upper()[-2:]}/participants?specialisation=OFFLINE"
-            data["heure"] = self.time[index]
-            data["number"] = number
-            data["nom_course"] = self.name_run[index]
+            data[index]["heure"] = self.time[index]
+            data[index]["number"] = number
+            data[index]["nom_course"] = self.name_run[index]
 
             r = requests.get(url)
 
             if r.ok:
                 result_json = r.json()["participants"]
-
                 for element in result_json:
-                    data["participants"]["nom"].append(element["nom"])
-                    data["participants"]["age"].append(element["age"])
-                    data["participants"]["numPmu"].append(element["numPmu"])
-                    data["participants"]["nombre course"].append(
-                        element["nombreCourses"])
-                    data["participants"]["nombre placée"].append(
-                        element["nombrePlaces"])
-                    data["participants"]["nombre placée second"].append(
-                        element["nombrePlacesSecond"])
-                    data["participants"]["nombre placée troisieme"].append(
-                        element["nombrePlacesTroisieme"])
-                    data["participants"]["nombre victoire"].append(
-                        element["nombreVictoires"])
-                    prono = Model.Pronostic(data)
+                    data[index]["participants"]["nom"].append(element["nom"])
+                    data[index]["participants"]["age"].append(element["age"])
+                    data[index]["participants"]["numPmu"].append(element["numPmu"])
+                    data[index]["participants"]["nombre course"].append(element["nombreCourses"])
+                    data[index]["participants"]["nombre placée"].append(element["nombrePlaces"])
+                    data[index]["participants"]["nombre placée second"].append(element["nombrePlacesSecond"])
+                    data[index]["participants"]["nombre placée troisieme"].append(element["nombrePlacesTroisieme"])
+                    data[index]["participants"]["nombre victoire"].append(element["nombreVictoires"])
+
+                    prono = Model.Pronostic(data, index)
                     data_with_prono = prono.pourcent_win() #retorune le nouveuax dictionnaire avec les pourcenatage de gagne est de placement pour cahque joueur  
-                self.save_data(data_with_prono)
-        return "Données sauvegarder"
+            prono.more_chance_win()
+            print()
+        # self.save_data(data_with_prono)
+        return "Données sauvegarder" 
